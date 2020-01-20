c74b4a66e7474bcb4424506800ba536f01edeed5
Joao Boing <jhb@certi.org.br>
2020-01-20 10:00:27 -0300

    Created tests and added satellite images as assets
    Initial setup for tests and models, added both 10 and 20 squared meters images for testing. All images are a fraction of their original size as their purpose is for testing only. Tests currently validate expected errors, the resulting shape of the models result and it's expected numpy array type.

---
+++ models/total_suspended_solids_turbidity.py
@@ -0,0 +1,59 @@
+# ##################################################################################################
+# Copyright (c) 2020 - Fundação CERTI
+# All rights reserved.
+# ##################################################################################################
+
+import numpy
+import rasterio as rio
+import rasterio
+
+numpy.seterr(divide="ignore", invalid="ignore")
+
+
+def doxaran_et_al_2003(band8a, band3):
+
+    return band8a.astype(float) / band3
+
+
+def miller_mckee_2004(band4):
+
+    return band4
+
+
+def liu_et_al_2006(band8a, band4):
+
+    return (band4.astype(float) -
+            band8a.astype(float)) / (band4 + band8a)
+
+
+def doxaran_et_al_2009(band8a, band4):
+
+    return band8a.astype(float) / band4
+
+
+def tarrant_et_al_2010(band8a, band4):
+
+    return band4.astype(float) - band8a
+
+
+def zhang_et_al_2010(band4, band2):
+
+    return (band2.astype(float) -
+            band4.astype(float)) / (band2 + band4)
+
+
+def tang_et_al_2010(band3, band2):
+
+    return band3.astype(float) / band2
+
+
+def create_tif(result, name):
+    meta_path = "tests/assets/20m/T22JCQ_20190806T133231_B03_20m.jp2"
+
+    meta = rio.open(meta_path).meta
+    print(meta)
+    meta.update(driver="GTiff")
+    meta.update(dtype=rasterio.float32)
+
+    with rio.open(name+".tif", "w", **meta) as dist:
+        dist.write(result.astype(rio.float32))
---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+import rasterio as rio
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_bands():
+    prefix_20m = 'tests/assets/20m/'
+    suffix_20m = '_20m_20181224.tif'
+    prefix_10m = 'tests/assets/10m/'
+    suffix_10m = '_10m_20181224.tif'
+
+    B02_20m = rio.open(prefix_20m+'B2'+suffix_20m).read()
+    B03_20m = rio.open(prefix_20m+'B3'+suffix_20m).read()
+    B04_20m = rio.open(prefix_20m+'B4'+suffix_20m).read()
+    B8A_20m = rio.open(prefix_20m+'B8A'+suffix_20m).read()
+
+    B02_10m = rio.open(prefix_10m+'B2'+suffix_10m).read()
+    B03_10m = rio.open(prefix_10m+'B3'+suffix_10m).read()
+    B04_10m = rio.open(prefix_10m+'B4'+suffix_10m).read()
+
+    bands = {
+        "20m": {
+            'B02': B02_20m,
+            'B03': B03_20m,
+            'B04': B04_20m,
+            'B8A': B8A_20m
+        },
+        "10m": {
+            'B02': B02_10m,
+            'B03': B03_10m,
+            'B04': B04_10m
+        }
+    }
+
+    return bands
