b98d7a1e7e59993e88b53d31d5b433ec2932c73d
fcs <fcs@certi.org.br>
2020-06-16 17:15:59 -0300

    Add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="qda_modelos",
     packages=["qda_modelos"],
     version="1.0.0",
     license="BSD",
     description="Implement bio-optic models to evaluate water quality indexes with satellite images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="CERTI Foundation",
     author_email="qda-pypi@certi.org.br",
     url="https://github.com/fundacaocerti/qda_modelos",
@@ -20,11 +25,7 @@ setup(
         "water",
         "remote sensing",
     ],
-    install_requires=[
-        "numpy",
-        "qda_modelos"
-        "rasterio",
-    ],
+    install_requires=["numpy", "qda_modelos" "rasterio",],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
