fde30909850935651c522c68e30ff1ee90ff46ff
fcs <fcs@certi.org.br>
2020-06-16 16:49:52 -0300

    Fix missing quotation mark

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3-Clause License,
+        "License :: OSI Approved :: BSD 3-Clause License",
         "Operating System :: OS Independent",
     ],
 )
