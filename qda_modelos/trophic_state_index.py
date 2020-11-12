328ca4abf87e286dc5305accdd45713862ad9f00
Edmar Pessoa Araujo Neto <ept@certi.org.br>
2020-11-12 11:23:29 -0300

    Fixing typo on the formula
    Just fixing this little (HUGE!) typo in the formula that messed
    everything up. Thanks @jhb-certi!

--- qda_modelos/trophic_state_index.py
+++ qda_modelos/trophic_state_index.py
@@ -13,4 +13,4 @@ def lamparelli_2004(chlorophylla, phosphorus):
     trophic_state_index_chl = 10 * (6 - ((0.92 - 0.34 * numpy.log(chlorophylla)) / numpy.log(2)))
     trophic_state_index_pho = 10 * (6 - (1.77 - 0.42 * (numpy.log(phosphorus) / numpy.log(2))))
 
-    return (trophic_state_index_chl / trophic_state_index_pho) / 2
+    return (trophic_state_index_chl + trophic_state_index_pho) / 2
